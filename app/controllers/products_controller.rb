9945fcdc5eb55bec6700aed8478af579831905bb
nicocadq <nmachado.camejo@gmail.com>
2024-06-12 02:39:12 -0300

    feat(product): add crud

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,50 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path, notice: 'Tu producto se ha creado correctamente'
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to products_path, notice: 'Tu producto se ha actualizado correctamente'
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente', status: :see_other
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
+  end
 end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,88 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  test 'render a list of products' do
+    get products_path
+
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'render a detailed product page' do
+    get product_path(products(:ps4))
+
+    assert_response :success
+    assert_select '.title', 'PS4 test'
+    assert_select '.description', "It's rota"
+    assert_select '.price', "200"
+  end
+
+  test 'render a new product form' do
+    get new_product_path
+
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to create a new product' do
+    post products_path, params: {
+      product: {
+        title: 'Nintendo 56',
+        description: 'Le faltan los cables',
+        price: 45
+      }
+    }
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Tu producto se ha creado correctamente'
+  end
+
+  test 'deost not allow to create a new product with empty fields' do
+    post products_path, params: {
+      product: {
+        title: '',
+        description: 'Le faltan los cables',
+        price: 45
+      }
+    }
+
+    assert_response :unprocessable_content
+  end
+
+  test 'render a edit product form' do
+    get edit_product_path(products(:ps4))
+
+    assert_response :success
+    assert_select 'form'
+  end
+
+  test 'allow to update a product' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: 50
+      }
+    }
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Tu producto se ha actualizado correctamente'
+  end
+
+  test 'deost not allow to update a product with an invalid field' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: nil
+      }
+    }
+
+    assert_response :unprocessable_content
+  end
+
+  test 'can delete products' do
+    assert_difference('Product.count', -1) do
+      delete product_path(products(:ps4))
+    end
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Tu producto se ha eliminado correctamente'
+  end
+end
